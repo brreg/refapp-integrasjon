3f70561eacac421cb2b35553dbfdb0482dfb223b
exthro <exthro@brreg.no>
2023-03-20 12:21:54 +0100

    BTF-2447 * Endre AppSecurity

--- src/main/java/refappintegrasjon/config/AppSecurity.java
+++ src/main/java/refappintegrasjon/config/AppSecurity.java
@@ -1,13 +1,18 @@
 package refappintegrasjon.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.WebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
-public class AppSecurity extends WebSecurityConfigurerAdapter {
-    @Override
-    public void configure(WebSecurity web) {
-        web.ignoring().antMatchers("/**");
+@EnableWebSecurity
+public class AppSecurity {
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        // TODO: tilpass etter behov
+        http.authorizeHttpRequests().antMatchers("/**").permitAll();
+        return http.build();
     }
 }
